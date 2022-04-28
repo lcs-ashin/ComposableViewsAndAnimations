fc38791cbd21b9537aab6028a9caedb42db07c1b
lcs-ashin <ashin@lcs.on.ca>
2022-04-28 16:57:37 -0400

    Made properties controlling each hearts' shape (filled or not filled) and a boolean to check if the heart is already filled or not, so only one button can be selected at once.

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableDescriptionView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableDescriptionView.swift
@@ -12,6 +12,14 @@ struct CustomComposableDescriptionView: View {
     // MARK: Stored properties
     @State private var chooseColor: Color = .purple
     
+    @State private var purpleHeartType: String = "heart"
+    @State private var orangeHeartType: String = "heart"
+    @State private var yellowHeartType: String = "heart"
+    @State private var greenHeartType: String = "heart"
+    @State private var blueHeartType: String = "heart"
+
+    @State private var heartIsChosen: Bool = false
+    
     // MARK: Computed properties
     var body: some View {
         
@@ -36,44 +44,108 @@ struct CustomComposableDescriptionView: View {
                 HStack {
                     Spacer()
                     // Choose purple effect
-                    Image(systemName: "heart")
+                    Image(systemName: purpleHeartType)
                         .font(Font.system(size: 40))
                         .foregroundColor(.purple)
                         .onTapGesture {
-                            chooseColor = .purple
+                            if heartIsChosen == false {
+                                purpleHeartType = "heart.fill"
+                                heartIsChosen = true
+                                chooseColor = .purple
+                                
+                                orangeHeartType = "heart"
+                                yellowHeartType = "heart"
+                                greenHeartType = "heart"
+                                blueHeartType = "heart"
+                                
+                            } else {
+                                purpleHeartType = "heart"
+                                heartIsChosen = false
+                            }
                         }
                     
                     // Choose orange effect
-                    Image(systemName: "heart")
+                    Image(systemName: orangeHeartType)
                         .font(Font.system(size: 40))
                         .foregroundColor(.orange)
                         .onTapGesture {
-                            chooseColor = .orange
+                            if heartIsChosen == false {
+                                orangeHeartType = "heart.fill"
+                                heartIsChosen = true
+                                chooseColor = .orange
+                                
+                                purpleHeartType = "heart"
+                                yellowHeartType = "heart"
+                                greenHeartType = "heart"
+                                blueHeartType = "heart"
+                                
+                            } else {
+                                orangeHeartType = "heart"
+                                heartIsChosen = false
+                            }
                         }
                     
                     // Choose yellow effect
-                    Image(systemName: "heart")
+                    Image(systemName: yellowHeartType)
                         .font(Font.system(size: 40))
                         .foregroundColor(.yellow)
                         .onTapGesture {
-                            chooseColor = .yellow
+                            if heartIsChosen == false {
+                                yellowHeartType = "heart.fill"
+                                heartIsChosen = true
+                                chooseColor = .yellow
+                                
+                                purpleHeartType = "heart"
+                                orangeHeartType = "heart"
+                                greenHeartType = "heart"
+                                blueHeartType = "heart"
+                                
+                            } else {
+                                yellowHeartType = "heart"
+                                heartIsChosen = false
+                            }
                         }
                     
                     // Choose green effect
-                    Image(systemName: "heart")
+                    Image(systemName: greenHeartType)
                         .font(Font.system(size: 40))
                         .foregroundColor(.green)
                         .onTapGesture {
-                            chooseColor = .green
+                            if heartIsChosen == false {
+                                greenHeartType = "heart.fill"
+                                heartIsChosen = true
+                                chooseColor = .green
+                                
+                                purpleHeartType = "heart"
+                                orangeHeartType = "heart"
+                                yellowHeartType = "heart"
+                                blueHeartType = "heart"
+                                
+                            } else {
+                                greenHeartType = "heart"
+                                heartIsChosen = false
+                            }
                         }
                     
                     // Choose blue effect
-                    Image(systemName: "heart")
+                    Image(systemName: blueHeartType)
                         .font(Font.system(size: 40))
                         .foregroundColor(.blue)
                         .onTapGesture {
-                            chooseColor = .blue
-                        }
+                            if heartIsChosen == false {
+                                blueHeartType = "heart.fill"
+                                heartIsChosen = true
+                                chooseColor = .blue
+                                
+                                purpleHeartType = "heart"
+                                orangeHeartType = "heart"
+                                yellowHeartType = "heart"
+                                greenHeartType = "heart"
+                                
+                            } else {
+                                blueHeartType = "heart"
+                                heartIsChosen = false
+                            }                        }
                     Spacer()
                 }
                 
